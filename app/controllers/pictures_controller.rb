5539d037497f108ae63b09172f00b56e7ace830d
nanako kan <n.k.0213.0529@gmail.com>
2020-04-19 13:03:46 +0900

    add: picture投稿機能の実装

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,17 @@
+class PicturesController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def new
+    @picture = Picture.new
+  end
+  def create
+    Picture.create(picture_params)
+    redirect_to new_picture_path
+  end
+  private
+  def picture_params
+    params.require(:picture).permit(:title, :content)
+  end
+end
